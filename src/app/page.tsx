66acc7d9a606189cb2e7a1a4c52ca9541f3aaa97
Velan Thever <thevarvelan@gmail.com>
2025-04-22 18:15:51 +0530

    Changed images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import logo from "@/assets/logo.png";
-import resumePreview from "@/assets/resume-preview.jpg";
+import resumePreview from "@/assets/image.png";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
